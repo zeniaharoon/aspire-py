bbcdc044a84f9aed701d6e79efb1dce4d66397f6
Zenia Haroon <zeniaharoon@gmail.com>
2023-06-30 17:07:48 -0400

    completed project0

--- Project-0/src/main.py
+++ Project-0/src/main.py
@@ -1,1 +1,124 @@
-print("hello world")
+print("Hello World!");
+
+total 8
+drwxr-xr-x   7 haroozr1  staff   224 Jun 30 13:41 .
+drwx------+  5 haroozr1  staff   160 Jun 30 13:42 ..
+drwxr-xr-x  13 haroozr1  staff   416 Jun 30 13:41 .git
+drwxr-xr-x   4 haroozr1  staff   128 Jun 30 13:41 Project-0
+drwxr-xr-x   3 haroozr1  staff    96 Jun 30 13:41 Project-1
+-rw-r--r--   1 haroozr1  staff  1413 Jun 30 13:41 README.md
+drwxr-xr-x   3 haroozr1  staff    96 Jun 30 13:41 tests
+
+ _________________________________________ 
+/ The difference between science and the  \
+| fuzzy subjects is that science requires |
+| reasoning while those other subjects    |
+| merely require scholarship.             |
+|                                         |
+\ -- Robert Heinlein                      /
+ ----------------------------------------- 
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+ _________________________________________ 
+/ The difference between science and the  \
+| fuzzy subjects is that science requires |
+| reasoning while those other subjects    |
+| merely require scholarship.             |
+|                                         |
+\ -- Robert Heinlein                      /
+ ----------------------------------------- 
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+ _________________________________________ 
+/ The difference between science and the  \
+| fuzzy subjects is that science requires |
+| reasoning while those other subjects    |
+| merely require scholarship.             |
+|                                         |
+\ -- Robert Heinlein                      /
+ ----------------------------------------- 
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+ _________________________________________ 
+/ The difference between science and the  \
+| fuzzy subjects is that science requires |
+| reasoning while those other subjects    |
+| merely require scholarship.             |
+|                                         |
+\ -- Robert Heinlein                      /
+ ----------------------------------------- 
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+
+ _________________________________________ 
+/ The difference between science and the  \
+| fuzzy subjects is that science requires |
+| reasoning while those other subjects    |
+| merely require scholarship.             |
+|                                         |
+\ -- Robert Heinlein                      /
+ ----------------------------------------- 
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+
+ _________________________________________ 
+/ The difference between science and the  \
+| fuzzy subjects is that science requires |
+| reasoning while those other subjects    |
+| merely require scholarship.             |
+|                                         |
+\ -- Robert Heinlein                      /
+ ----------------------------------------- 
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+ _________________________________________ 
+/ The difference between science and the  \
+| fuzzy subjects is that science requires |
+| reasoning while those other subjects    |
+| merely require scholarship.             |
+|                                         |
+\ -- Robert Heinlein                      /
+ ----------------------------------------- 
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+ _________________________________________ 
+/ The difference between science and the  \
+| fuzzy subjects is that science requires |
+| reasoning while those other subjects    |
+| merely require scholarship.             |
+|                                         |
+\ -- Robert Heinlein                      /
+ ----------------------------------------- 
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
